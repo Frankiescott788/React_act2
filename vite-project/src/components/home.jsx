e936285fe0cacacc2c8b79f153961479f3234574
Frankiescott788 <scottfrankie864@gmail.com>
2024-07-16 11:44:19 +0200

    first commit

---
+++ vite-project/src/components/home.jsx
@@ -0,0 +1,30 @@
+import { Fragment } from "react";
+
+const Home = () => {
+    return ( 
+        <div className="parent-element">
+            <div className="children">
+                <div>
+                <div className="row-1">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="row-2">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="row-1">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                </div>
+                
+            </div>
+        </div>
+     );
+}
+ 
+export default Home;
